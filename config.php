9b5619384fadc8e93d538ab50a51863383bb8ea6
Torset <88652786+Torset@users.noreply.github.com>
2022-02-15 10:06:45 +0100

    Update config.php

--- config.php
+++ config.php
@@ -7,4 +7,12 @@ $dbpassw = '';
 $frontpage_limit = "500";
 $webhost = '10.212.139.37';
 $weburl = 'http://' . $webhost ;
+
+if ( getenv("BF_MEMCACHE_SERVER")){
+   $memcache_enabled_pictures = 1;
+   $memcache_server = getenv("BF_MEMCACHE_SERVER");
+   $memcache_enabled = 1;
+}
+
+
 ?>
