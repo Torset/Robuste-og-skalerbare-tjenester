1d46f252b07c80b024e3fae4dcb80d7d2f57d0db
Torset <88652786+Torset@users.noreply.github.com>
2022-02-15 12:38:41 +0100

    Update config.php

--- config.php
+++ config.php
@@ -5,9 +5,11 @@ $db = "bf";
 $dbuser = "t2user";
 $dbpassw = '';
 $frontpage_limit = "500";
+if ( getenv("BF_FRONTPAGE_LIMIT") ){
+   $frontpage_limit = getenv("BF_FRONTPAGE_LIMIT");
+}
 $webhost = '10.212.139.37';
 $weburl = 'http://' . $webhost ;
-
 if ( getenv("BF_MEMCACHE_SERVER")){
    $memcache_enabled_pictures = 1;
    $memcache_server = getenv("BF_MEMCACHE_SERVER");
